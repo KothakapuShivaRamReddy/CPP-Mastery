a2d8921c93c17e1987892331d24305fcbc55b3eb
KothakapuShivaRamReddy <shivakothakapu191818@gmail.com>
2026-02-21 00:12:47 +0530

    feat: added new codes

---
+++ d8/example.cpp
@@ -0,0 +1,17 @@
+#include<iostream> 
+#include<string.h>
+using namespace std;
+
+class Example{
+    public:
+       void demo(){
+        cout<<"this is demo";
+       }
+};
+
+int main(){
+    Example e;
+    e.demo();
+    return 0;
+
+}
